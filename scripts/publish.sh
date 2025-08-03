16b8b7ca558177d34055f63fa7b799491b190a1d
RILIEVO Axel <arilievo@aol.com>
2025-08-03 13:35:29 +0000

    Fix: update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-# 1) Publish du projet Web dans ServerIIS/build
 cd ../YourProjectName
-dotnet publish src/Web/Web.csproj \
-  --configuration Debug \
-  --output ../ServerIIS/build \
-  /p:PublishProfile=Dev
+dotnet publish src/Web/Web.csproj /p:PublishProfile=Local
